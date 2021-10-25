941f019bd10e8db479fba98671fb77be7d11352e
kykys <neskajju@mail.ru>
2021-10-26 00:13:37 +0300

    Add temp file with new cut_file function

---
+++ Srcs/cut_file.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cut_file.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wgaunt <wgaunt@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/26 00:11:36 by wgaunt            #+#    #+#             */
+/*   Updated: 2021/10/26 00:14:88 by wgaunt           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*take_file(char *redir_p, char *endfile_p)
+{
+	char	*file;
+	char	*tmp;
+
+	while (*redir_p == '<' || *redir_p == '>')
+		redir_p++;
+	while (*redir_p == ' ')
+		redir_p++;
+	file = (char *)ft_calloc(endfile_p - redir_p + 1, sizeof(char));
+	if (!file)
+		return (0);
+	tmp = file;
+	while (redir_p != endfile_p)
+	{
+		*file = *redir_p;
+		redir_p++;
+		file++;
+	}
+	*file = 0;
+	file = ft_strtrim(tmp, " ");
+	free(tmp);
+	if (!file)
+		return (0);
+	return (file);
+}
+
+char	*take_com(char *str, char *redir_p, char *endfile_p)
+{
+	char	*com;
+
+	*redir_p = 0;
+	com = ft_strjoin(str, endfile_p);
+	free(str);
+	return (com);
+}
+
+int	cut_file(char **str, char **file, int type)
+{
+	char	*redir_p;
+	char	*endfile_p;
+
+	if (type == R_RDR || type == DR_RDR)
+		redir_p = ft_strchrq(*str, '>');
+	else
+		redir_p = ft_strchrq(*str, '<');
+	endfile_p = redir_p;
+	while (*endfile_p == '>' || *endfile_p == '<')
+		endfile_p++;
+	while (*endfile_p == ' ')
+		endfile_p++;
+	endfile_p = ft_strchrq(endfile_p, ' ');
+	if (endfile_p == 0)
+		endfile_p = redir_p + ft_strlen(redir_p);
+	*file = take_file(redir_p, endfile_p);
+	*str = take_com(*str, redir_p, endfile_p);
+	if (!*file || *str)
+		return (-1);
+	return (0);
+}
